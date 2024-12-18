9fdb67baecf365f2584dbeaa2146d11caf779acc
Eigeen <dengyk2002@qq.com>
2024-12-18 23:58:32 +0800

    修正模块扫描路径设置

--- src/utility.rs
+++ src/utility.rs
@@ -1,6 +1,15 @@
-use windows::{core::PCWSTR, Win32::System::LibraryLoader::AddDllDirectory};
+use std::sync::atomic::{self, AtomicBool};
+
+use windows::{
+    core::PCWSTR,
+    Win32::System::LibraryLoader::{
+        AddDllDirectory, SetDefaultDllDirectories, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS,
+    },
+};
 
 use crate::error::Error;
+
+static DEFAULT_SETTED: AtomicBool = AtomicBool::new(false);
 
 /// 将字符串转换为 UTF16-LE 字节数组，有 \0 结尾
 pub fn to_wstring_bytes_with_nul(s: &str) -> Vec<u16> {
@@ -9,7 +18,15 @@ pub fn to_wstring_bytes_with_nul(s: &str) -> Vec<u16> {
 
 /// 添加 DLL 加载目录
 pub fn add_dll_directory(rel_path: &str) -> Result<(), Error> {
+    if !DEFAULT_SETTED.load(atomic::Ordering::SeqCst) {
+        unsafe {
+            SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS)?;
+        }
+        DEFAULT_SETTED.store(true, atomic::Ordering::SeqCst);
+    }
+
     let abs_path = std::fs::canonicalize(rel_path)?;
+    log::debug!("Adding DLL directory: {}", abs_path.display());
     let wstr = to_wstring_bytes_with_nul(abs_path.to_string_lossy().as_ref());
 
     unsafe {
